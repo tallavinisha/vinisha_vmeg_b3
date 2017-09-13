eb1dac3cb9929f8534001e248531c20dc38038e4
tallavinisha <31915063+tallavinisha@users.noreply.github.com>
2017-09-13 13:05:25 +0530

    Add files via upload

--- StudentGroup.java
+++ StudentGroup.java
@@ -26,7 +26,11 @@ public class StudentGroup implements StudentArrayOperation {
 	@Override
 	public Student[] getStudents() {
 		// Add your implementation here
-		return null;
+//return null;
+if(students!=null)
+return students;
+else
+return null;
 	}
 
 	@Override
